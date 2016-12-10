1292baa1321dcb9bb1045a018bf53d5353cb7327
Unknown <sakurawlzxy@gmail.com>
2016-12-11 01:45:37 +0800

    更新翻译配置文件

---
+++ src/res/translator/translator_cn.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindows</name>
+    <message>
+        <location filename="../../main/mainwindows.ui" line="14"/>
+        <source>CheckForDuplicateFiles</source>
+        <translation>重复文件检查</translation>
+    </message>
+</context>
+</TS>
