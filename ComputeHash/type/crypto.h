3ba3be7c8f914c3ed1d694bfe82499e20b277e85
Unknown <sakurawlzxy@gmail.com>
2017-02-03 14:59:35 +0800

    modify : 修改了SHA1计算模块

--- ComputeHash/type/crypto.h
+++ ComputeHash/type/crypto.h
@@ -11,45 +11,36 @@ class Crypto : public Compute
 {
 public:
     explicit Crypto (T type)
-        :m_crypto(new QCryptographicHash(type) )
     {
         switch (type)
         {
         case QCryptographicHash::Sha1:
             m_hashType = util::SHA1;
             m_typeName = "SHA1";
+            m_crypto = new QCryptographicHash(QCryptographicHash::Sha1);
             break;
         case QCryptographicHash::Md5:
+        default:
             m_hashType = util::MD5;
             m_typeName = "MD5";
-            break;
-        default:
-            m_hashType = util::NOTYPE;
-            m_typeName = "NoType";
+            m_crypto = new QCryptographicHash(QCryptographicHash::Md5);
             break;
         }
     }
 
     ~Crypto()
     {
-        if(nullptr != m_crypto)
-            delete m_crypto;
+        delete m_crypto;
     }
 
     void update(QByteArray &data, QString oldComputeHash)
     {
-        if(nullptr == m_crypto)
-            return;
-
         Q_UNUSED(oldComputeHash)
         m_crypto->addData(data);
     }
 
     QString getFinalResult()
     {
-        if(nullptr == m_crypto)
-            return QString();
-
         return QString(m_crypto->result().toHex().toUpper());
     }
 
